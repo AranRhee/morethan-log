66929759f131a481ae586b90f711234724e12fdc
ARAN <aranrhee@icloud.com>
2023-11-06 14:41:15 +0900

    mofied setting

--- site.config.js
+++ site.config.js
@@ -10,12 +10,12 @@ const CONFIG = {
     github: "AranRhee",
     instagram: "",
   },
-  projects: [
-    {
-      // name: `morethan-log`,
-      // href: "https://github.com/morethanmin/morethan-log",
-    },
-  ],
+  // projects: [
+  //   {
+  //     name: `morethan-log`,
+  //     href: "https://github.com/morethanmin/morethan-log",
+  //   },
+  // ],
   // blog setting (required)
   blog: {
     title: "Dev Log😎",
